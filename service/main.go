744f27fc30080776531fba8660a5ce7ae47663fd
sbagnallsta <samuel.bagnall@statravel.com>
2017-09-29 15:12:04 +0100

    viper

--- service/main.go
+++ service/main.go
@@ -1,39 +1,55 @@
 package main 
 
 import (
-	// "log"
-	// "time"
-	// "gopkg.in/mgo.v2"
+	"os"
+	"fmt"
+	"log"
+	"time"
+	"gopkg.in/mgo.v2"
 	"github.com/gin-gonic/gin"
-	"github.com/sbagnallsta/flights-history-service-test-2/service/profiles"
-	// "github.com/sbagnallsta/flights-history-service-test-2/service/models/dbConnector"
+	// "github.com/sbagnallsta/flights-history-service-test-2/service/profiles"
+	"github.com/spf13/viper"
+	"github.com/sbagnallsta/flights-history-service-test-2/service/models/dbConnector"
 	"github.com/sbagnallsta/flights-history-service-test-2/service/controllers/health"
-	// "github.com/sbagnallsta/flights-history-service-test-2/service/controllers/flights"
+	"github.com/sbagnallsta/flights-history-service-test-2/service/controllers/flights"
 )
 
 func main() {
-	config := profiles.LoadConfiguration()
+	// config := profiles.LoadConfiguration()
+	profile := os.Getenv("FLIGHTS_HISTORY_SERVICE_PROFILE")
+	if(profile == "") {
+		profile = "local"
+		fmt.Println(profile)
+	}	
+	viper.SetConfigName(profile)
+	viper.AddConfigPath("./profiles")
+	err := viper.ReadInConfig()
+	if err != nil {
+		fmt.Println("Error", err)
+	}
 	router := gin.Default()
 
-	// mongoInfo := &mgo.DialInfo {
-	// 	Addrs: []string{config.DataSource.Url},
-	// 	Timeout: 60 * time.Second,
-	// }
+	mongoInfo := &mgo.DialInfo {
+		// Addrs: []string{config.DataSource.Url},
+		Addrs: []string{viper.GetString("DataSource.Url")},
+		Timeout: 60 * time.Second,
+	}
 
-	// session, err := mgo.DialWithInfo(mongoInfo)
+	session, err := mgo.DialWithInfo(mongoInfo)
 
-	// if err != nil {
-	// 	log.Fatal("Failed to connect to mongoDB ", err)
-	// }
+	if err != nil {
+		log.Fatal("Failed to connect to mongoDB ", err)
+	}
 
 	// db := dbConnector.DBData{config.DataSource.Database, config.DataSource.Collection, session}
+	db := dbConnector.DBData{viper.GetString("DataSource.Database"), viper.GetString("DataSource.Collection"), session}
 
 	router.GET("/health", healthController.Health)
 
-	// flights := router.Group("/flights")
-	// {
-	// 	// flights.POST("", flightController.StoreFlights(db))
-	// }
+	flights := router.Group("/flights")
+	{
+		flights.POST("", flightController.StoreFlights(db))
+	}
 
-	router.Run(config.Port)
+	router.Run(viper.GetString("Port"))
 }
--- service/profiles/profiles.go
+++ service/profiles/profiles.go
@@ -1,66 +1,66 @@
-package profiles
+// package profiles
 
-import (
-	"log"
-	"encoding/json"
-	"os"
-)
+// import (
+// 	"log"
+// 	"encoding/json"
+// 	"os"
+// )
 
-const fileExtension = ".json"
-const local = "local"
+// const fileExtension = ".json"
+// const local = "local"
 
-type Configuration struct {
-	Port               string
-	DataSource              struct {
-		Url string
-		Database string
-		Collection string
-	}
-}
+// type Configuration struct {
+// 	Port               string
+// 	DataSource              struct {
+// 		Url string
+// 		Database string
+// 		Collection string
+// 	}
+// }
 
-func LoadConfiguration() Configuration {
+// func LoadConfiguration() Configuration {
 
-	var config Configuration
-	profile := getProfile()
-	file := concatString(profile, fileExtension)
-	relativeProfilePath := getGoPath(profile)
-	absoluteProfilePath := concatString(os.Getenv("GOPATH"), relativeProfilePath)
-	configFile, err := os.Open(concatString(absoluteProfilePath, file))
-	defer configFile.Close()
-	if err != nil {
-		log.Println(err)
-	}
-	jsonParser := json.NewDecoder(configFile)
-	jsonParser.Decode(&config)
-	return config
-}
+// 	var config Configuration
+// 	profile := getProfile()
+// 	file := concatString(profile, fileExtension)
+// 	relativeProfilePath := getGoPath(profile)
+// 	absoluteProfilePath := concatString(os.Getenv("GOPATH"), relativeProfilePath)
+// 	configFile, err := os.Open(concatString(absoluteProfilePath, file))
+// 	defer configFile.Close()
+// 	if err != nil {
+// 		log.Println(err)
+// 	}
+// 	jsonParser := json.NewDecoder(configFile)
+// 	jsonParser.Decode(&config)
+// 	return config
+// }
 
-func getProfile() string {
+// func getProfile() string {
 
-	profile := os.Getenv("FLIGHTS_HISTORY_SERVICE_PROFILE")
+// 	profile := os.Getenv("FLIGHTS_HISTORY_SERVICE_PROFILE")
 
-	if profile == "" {
-		profile = local
-	}
+// 	if profile == "" {
+// 		profile = local
+// 	}
 
-	return profile
+// 	return profile
 
-}
+// }
 
-func getGoPath(profile string) string {
+// func getGoPath(profile string) string {
 
-	if profile == local {
+// 	if profile == local {
 
-		return "\\src\\github.com\\sbagnallsta\\flights-history-service-test-2\\service\\profiles\\"
+// 		return "\\src\\github.com\\sbagnallsta\\flights-history-service-test-2\\service\\profiles\\"
 
-	}
+// 	}
 
-	return "/src/github.com/sbagnallsta/flights-history-service-test-2/service/profiles/"
+// 	return "/src/github.com/sbagnallsta/flights-history-service-test-2/service/profiles/"
 
-}
+// }
 
-func concatString(string1 string, string2 string) string {
+// func concatString(string1 string, string2 string) string {
 
-	return string1 + string(string2)
+// 	return string1 + string(string2)
 
-}
+// }
